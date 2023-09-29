d6577e30a995f9c89b9c744a0ca254d6dca0f9f7
jonnymd <jonny.brennan@gmail.com>
2023-09-29 23:07:04 +0000

    Initial page

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// For future dynamic interactions, like a smooth scroll or modal pop-ups for form submissions
+document.getElementById('requestDemo').addEventListener('click', function() {
+    alert('Demo request feature not implemented yet!');
+});
